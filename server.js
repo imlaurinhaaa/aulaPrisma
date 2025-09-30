68b73c64a5a729989a415b63a2991bb12c8650ac
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-09-30 10:23:57 -0300

    feat: config server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import router from "./src/routes/index.routes.js"
 
 config(); // Carrega variáveis de ambiente do arquivo .env
 const port = process.env.PORT || 5000;
@@ -7,13 +8,9 @@ const port = process.env.PORT || 5000;
 // Inicializa o Express
 const app = express();
 
-app.use(express.json()); // Parse de JSON
+app.use(express.json()); // Parse corpo de JSON
 
-// Rota base para verificar se o servidor está rodando
-app.get("/", (req, res) => {
-  res.json({ message: "API de Coleção de Animes funcionando!" });
-});
-
+app.use("/", router);
 // Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import movieRouter from './movies.routes.js';
 
 const router = express.Router();
+
+router.use("/movies", movieRouter);
 
 export default router;
